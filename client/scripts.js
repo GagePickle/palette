1c0028efe416d1cf118de550706c03fd32806b8e
Manav Misra <manavm@visionwebsoft.com>
2020-09-24 20:25:53 -0500

    Add functionality to gray out the current palette for better visually appeal

--- client/scripts.js
+++ client/scripts.js
@@ -4,6 +4,16 @@ const palettes = document.querySelectorAll("div > div");
 // computedStyles will include all of the 'numbered color properties'
 const computedStyles = getComputedStyle(palettes[0]);
 
+const resetPalette = () => {
+  // This will be on 'null' on first click
+  const grayed = document.querySelector(".grayscale");
+
+  // This will prevent console error when 'grayed' is null on first click
+  if (grayed) {
+    grayed.classList.remove("grayscale");
+  }
+};
+
 // Use the index of the 'palette' to assign a color from custom properties
 palettes.forEach((palette, index) => {
   palette.style.setProperty(
@@ -12,6 +22,8 @@ palettes.forEach((palette, index) => {
   );
 
   palette.addEventListener("click", () => {
+    resetPalette();
+
     display.style.setProperty(
       "background-color",
       palette.style.getPropertyValue("background-color")
