d43e2ad18ab2feb77f582a406ed230b715a52e2a
Manav Misra <manavm@visionwebsoft.com>
2020-09-24 19:55:33 -0500

    Refactor out unecessary 'querySelector'

--- client/scripts.js
+++ client/scripts.js
@@ -2,7 +2,7 @@ const display = document.querySelector("main > div");
 const palettes = document.querySelectorAll("div > div");
 
 // computedStyles will include all of the 'numbered color properties'
-const computedStyles = getComputedStyle(document.querySelector("div > div"));
+const computedStyles = getComputedStyle(palettes[0]);
 
 // Use the index of the 'palette' to assign a color from custom properties
 palettes.forEach((palette, index) => {
