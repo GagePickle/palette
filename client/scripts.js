7ecf141b94e582ea3ab4b1f67acf90bb5fa6f4a5
Gage Pickle <gagepickle@yahoo.com>
2020-10-01 20:09:12 -0500

    Refactor to make code more declarative

--- client/scripts.js
+++ client/scripts.js
@@ -4,6 +4,27 @@ const palettes = document.querySelectorAll("div > div");
 // computedStyles will include all of the 'numbered color properties'
 const computedStyles = getComputedStyle(palettes[0]);
 
+const addPaletteColor = (palette, index) => {
+  // Generates the palette
+  palette.style.setProperty(
+    "background-color",
+    computedStyles.getPropertyValue(`--${index}`)
+  );
+
+  palette.addEventListener("click", () => {
+    resetPalette();
+
+    display.style.setProperty(
+      // Which property
+      "background-color",
+      // What is the value? Get it from the 'indexed' color variables
+      palette.style.getPropertyValue("background-color")
+    );
+
+    palette.classList.add("grayscale");
+  });
+};
+
 const resetPalette = () => {
   // This will be on 'null' on first click
   const grayed = document.querySelector(".grayscale");
@@ -15,20 +36,4 @@ const resetPalette = () => {
 };
 
 // Use the index of the 'palette' to assign a color from custom properties
-palettes.forEach((palette, index) => {
-  palette.style.setProperty(
-    "background-color",
-    computedStyles.getPropertyValue(`--${index}`)
-  );
-
-  palette.addEventListener("click", () => {
-    resetPalette();
-
-    display.style.setProperty(
-      "background-color",
-      palette.style.getPropertyValue("background-color")
-    );
-
-    palette.classList.add("grayscale");
-  });
-});
+palettes.forEach(addPaletteColor);
